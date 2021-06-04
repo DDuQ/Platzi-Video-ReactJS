2fd1927a9224ad71aad0102943626af4db3a86f3
Esteban Duque <esteban.duque@masglobalconsulting.com>
2021-06-04 14:23:27 -0500

    Redux implemented using actions and reducers.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout = ({children}) => (
-    <div className="App">
-        <Header />
-        {children}
-        <Footer />
-    </div>
+const Layout = ({ children }) => (
+  <div className='App'>
+    {children}
+    <Footer />
+  </div>
 );
 
-export default Layout
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { initialState } from '../initialState.json';
 import reducer from './reducers';
 import App from './routes/App';
 
-const store = createStore(reducer, initialState);
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, initialState, composeEnhancers);
 
 ReactDOM.render(
   <Provider store={store}>
